ee5e507e051d90502fc613d458d26771811f4370
GitPlayB <olivb.sh@gmail.com>
2020-01-05 21:04:42 +0800

    add one

---
+++ java/1.java
@@ -0,0 +1,22 @@
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            /* 
+            * 方法一：暴力破解
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == (nums[i] + nums[j])) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+            */
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put()
+            if ()
+        }
+
+        return result;
+    }
+}
