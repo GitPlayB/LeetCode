a20cb59cf19a0a34179f400a4863846d86a0eff8
libin <libin@xl.com>
2020-01-05 21:50:46 +0800

    add pom.xml

---
+++ src/main/java/TwoNumbers.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoNumbers {
+    public static void main(String[] args) {
+        Solution1 solution1 = new Solution1();
+        int[] r = new int[2];
+        int[] input = {2, 7, 11, 15};
+        int target = 9;
+        r = solution1.twoSum(input, target);
+        System.out.println(r[0] + " " + r[1]);
+    }
+}
+
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            /* 
+            * 方法一：暴力破解
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == (nums[i] + nums[j])) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+            */
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+            }
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
+}
